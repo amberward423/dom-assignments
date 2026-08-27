59b1be9ef1352edc4f328b8e65d5e47141df87da
amberward423 <amberward423@gmail.com>
2026-08-27 09:35:32 +0300

    beginning of distance calculation

--- t4/t4.js
+++ t4/t4.js
@@ -770,38 +770,18 @@ const restaurants = [
   },
 ];
 
+ {
+    location: {type: 'Point', coordinates: [25.018456, 60.228982]},
+    _id: '6470d38ecb12107db6fe24c1',
+    companyId: 68,
+    name: 'Ravintola Ladonlukko',
+    address: 'Latokartanonkaari 9 A',
+    postalCode: '00790',
+    city: 'Helsinki',
+    phone:
+      '+358 50 4653899 Ravintolan esimies +358 50 435 8072 Kokoustarjoilut /ravintola',
+    company: 'Sodexo',
+    __v: 0,
+  },
+
 // your code here
-
-//{
-  //  location: {type: 'Point', coordinates: [24.903147, 60.221729]},
-   //  _id: '6470d38ecb12107db6fe24c2',
-   //  companyId: 1580536,
-   //  name: 'Ravintola Stadin AO Ilkantie',
-  //   address: 'Ilkantie 3',
-   //  postalCode: '00400',
-   //  city: 'Helsinki',
-   //  phone: '+358 (0) 50 4710 211',
-   //  company: 'Sodexo',
-  //   __v: 0,
-  // },
-
-  const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-function success(pos) {
-  const crd = pos.coords;
-
-  console.log("Your current position is:");
-  console.log(`Latitude: ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
